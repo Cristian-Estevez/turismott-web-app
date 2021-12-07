2819aa689e3a33abb554cfadaeca05823a8f7b24
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-12-07 16:59:01 -0300

    Funciona al comprar se guarda en tabla itinerario y no se le oferta más al cliente para su compra

--- ttt_webapp/src/main/java/service/ProductoService.java
+++ ttt_webapp/src/main/java/service/ProductoService.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import model.Atraccion;
@@ -8,6 +11,8 @@ import model.Promocion;
 import model.Usuario;
 import persistence.AtraccionDAO;
 import persistence.PromocionDAO;
+import persistence.commons.ProveedorDeConeccion;
+import utils.ProductosPorPreferencia;
 
 public class ProductoService {
 	
@@ -23,25 +28,62 @@ public class ProductoService {
 		productos.addAll(atracciones);
 		productos.addAll(promociones);
 		
-//		ArrayList<Producto> productosParaOfertar = new ArrayList<Producto>();
-//		
-//		productosParaOfertar = this.quitarYaComprados(productos, usuario);
-//		
-//		productosParaOfertar.sort(new ProductosPorPreferencia(usuario.getTipoAtraccionFavorita()));
+		this.cargarItinerario(usuario, atracciones, promociones);
 		
-		return productos;
+		ArrayList<Producto> productosParaOfertar = new ArrayList<Producto>();
+		
+		productosParaOfertar = this.quitarYaComprados(productos, usuario);
+		
+		productosParaOfertar.sort(new ProductosPorPreferencia(usuario.getTipoAtraccionFavorita()));
+		
+		return productosParaOfertar;
 	}
 
-//	private ArrayList<Producto> quitarYaComprados(ArrayList<Producto> productos, Usuario usuario) {
-//		ArrayList<Producto> productosParaOfertar = new ArrayList<Producto>();
-//
-//		ArrayList<Producto> productosCopy = new ArrayList<Producto>(productos);
-//
-//		for (Producto unProducto : productos) {
-//			if (usuario.yaCompro(unProducto)) {
-//				productosCopy.remove(unProducto);
-//			}
-//		}
-//		return productosCopy;		
-//	}
+	private void cargarItinerario(Usuario usuario, ArrayList<Atraccion> atracciones, ArrayList<Promocion> promociones) {
+		String query = "SELECT itinerario.usuario_id, itinerario.atraccion_id, itinerario.promocion_id FROM itinerario WHERE itinerario.usuario_id = ?";
+		
+		try {
+			Connection conn = ProveedorDeConeccion.getConeccion();
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setString(1, Integer.toString(usuario.getId()));
+			ResultSet resultado = statement.executeQuery();
+			
+			while(resultado.next()) {
+				int id = resultado.getInt(2);
+				
+				// el resultado es una Promoción
+				if (resultado.wasNull()) {
+					id = resultado.getInt(3);
+					for (Promocion unaPromocion : promociones) {
+						if (id == unaPromocion.getId()) {
+							usuario.actualizarItinerario(unaPromocion);
+						}
+					}
+				// el serultado es una Atraccion
+				} else {
+					for (Atraccion unaAtraccion : atracciones) {
+						if (id == unaAtraccion.getId()) {
+							usuario.actualizarItinerario(unaAtraccion);
+						}
+					}
+				}
+				usuario.actualizarCantidadDeProductosCompradaPreviamente();
+			}
+			
+		} catch (Exception e) {
+			System.err.println("Error cargando el itinerario el usuario " + usuario.getNombre() );
+		}
+	}
+
+	private ArrayList<Producto> quitarYaComprados(ArrayList<Producto> productos, Usuario usuario) {
+
+		ArrayList<Producto> productosCopy = new ArrayList<Producto>(productos);
+
+		for (Producto unProducto : productos) {
+			if (usuario.yaCompro(unProducto)) {
+				productosCopy.remove(unProducto);
+			}
+		}
+		return productosCopy;		
+	}
 }
