f6bc6ea93d55c243f23bedec81d4f3727a6f9c50
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-12-09 22:57:28 -0300

    Se comienza crearatraccionservlet

---
+++ ttt_webapp/src/main/java/controller/admin/CrearAtraccionServlet.java
@@ -0,0 +1,9 @@
+package controller.admin;
+
+import jakarta.servlet.http.HttpServlet;
+
+public class CrearAtraccionServlet extends HttpServlet {
+
+	private static final long serialVersionUID = -2046122623036539113L;
+
+}
